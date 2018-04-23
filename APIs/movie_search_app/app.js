78336cfaba939297d5df81febdbf4f0393604fa3
Mansur Muaz Ekici <mmuazekici@gmail.com>
2018-04-23 23:56:36 +0000

    search added to the movie app!

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -6,16 +6,33 @@ var request = require("request");
 app.set("view engine", "ejs");
 
 
+
+app.get("/", function(req, res) {
+   res.render("search"); 
+});
+
+
+
 app.get("/results" , function(req, res){
    
-   request("https://www.omdbapi.com/?s=star&&apikey=thewdb", function(error, response, body){
+   var query = req.query.movie
+   var url = "https://www.omdbapi.com/?s="+ query +"&&apikey=thewdb";
+   
+   request(url, function(error, response, body){
       if (!error && response.statusCode == 200) {
           var data = JSON.parse(body);
-          res.render("results", {data: data});
+          res.render("results", {data: data, movie: query});
       } 
    });
     
 });
+
+
+
+
+
+
+
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
