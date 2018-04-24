6e61762bb9f8dca07ad35d25a165e8876bfc9aae
Mansur Muaz Ekici <mmuazekici@gmail.com>
2018-04-24 06:34:42 +0000

    Styling Campgrounds

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -11,9 +11,15 @@ var campgrounds = [
     {name:"Yosemite", image:"https://jameskaiser.com/wp-content/uploads/2015/03/yosemite-camping.jpg"},
     {name:"Grand Canyon", image:"https://img-aws.ehowcdn.com/877x500p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/7d467d41aa0241e79e38a37286b1219d"},
     {name:"Erciyes", image:"https://www.outdoorhaber.com/wp-content/uploads/2012/02/erciyes_sutdonduran_kamp-1-990x556.jpg"},
+    {name:"Yosemite", image:"https://jameskaiser.com/wp-content/uploads/2015/03/yosemite-camping.jpg"},
+    {name:"Grand Canyon", image:"https://img-aws.ehowcdn.com/877x500p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/7d467d41aa0241e79e38a37286b1219d"},
+    {name:"Erciyes", image:"https://www.outdoorhaber.com/wp-content/uploads/2012/02/erciyes_sutdonduran_kamp-1-990x556.jpg"},
+    {name:"Antalya", image:"https://www.kolayyolculuk.com/blog/wp-content/uploads/2016/02/08-yazili-kanyon-kolay-yolculuk.jpg"},
+    {name:"Erciyes", image:"https://www.outdoorhaber.com/wp-content/uploads/2012/02/erciyes_sutdonduran_kamp-1-990x556.jpg"},
+    {name:"Antalya", image:"https://www.kolayyolculuk.com/blog/wp-content/uploads/2016/02/08-yazili-kanyon-kolay-yolculuk.jpg"},
+    {name:"Yosemite", image:"https://jameskaiser.com/wp-content/uploads/2015/03/yosemite-camping.jpg"},
+    {name:"Grand Canyon", image:"https://img-aws.ehowcdn.com/877x500p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/7d467d41aa0241e79e38a37286b1219d"},
     {name:"Antalya", image:"https://www.kolayyolculuk.com/blog/wp-content/uploads/2016/02/08-yazili-kanyon-kolay-yolculuk.jpg"}]
-
-
 
 
 
@@ -60,7 +66,9 @@ app.get("/campgrounds/new", function(req, res) {
 
 
 
-
+//====================================
+//            Listen the PORT
+//====================================
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("YelpCamp app server is started!");
