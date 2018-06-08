3f27734085b2d1f80a4a3f9ae7b4c3dd7d676d8c
Mansur Muaz Ekici <mmuazekici@gmail.com>
2018-06-08 07:42:46 +0000

    Associatians Added

---
+++ Associations/references.js
@@ -0,0 +1,50 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/blog_demo_2");
+ 
+var Post = require("./models/post");
+var User = require("./models/user");
+ 
+User.remove({}, function() {
+    Post.remove({}, function() {
+        User.create({
+            email: "bob@gmail.com",
+            name: "Bob Belcher"
+        }, function(err, user) {
+            if(err) {
+                console.log(err);
+            } else {
+                User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, foundUser){
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        console.log(foundUser);
+                        Post.create({
+                          title: "How to cook the best burger pt. 2",
+                          content: "blah blah blah blah blah"
+                        }, function(err, post){
+                            if(err) {
+                                console.log(err);
+                            } else {
+                                User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+                                    if(err){
+                                        console.log(err);
+                                    } else {
+                                        foundUser.posts.push(post._id);
+                                        foundUser.save(function(err, data){
+                                            if(err){
+                                                console.log(err);
+                                            } else {
+                                                console.log(data);
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+ 
+    });
+});
