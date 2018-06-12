5509e3ba88a40c78e14c1ae09ed45a277a3ad6ea
Mansur Muaz Ekici <mmuazekici@gmail.com>
2018-06-12 03:20:54 +0000

    Register Done

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -11,7 +11,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(require("express-session")({
     secret: "I still love you!",
     resave: false,
@@ -24,13 +24,41 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
+//=======================
+//---------ROUTES--------
+//=======================
 app.get("/", function(req, res){
     res.render("home");
 });
 
 app.get("/secret", function (req, res) {
     res.render("secret");
+});
+
+
+//--------Auth Routes--------
+
+//Showing SignUp page
+app.get("/register", function(req, res) {
+  res.render("register");  
+});
+
+//handling user sign up
+app.post("/register", function(req, res) {
+  
+  var username = req.body.username;
+  var password = req.body.password;
+  
+  User.register(new User({username: username}), password, function(err, newUser){
+      if (err) {
+          console.log("Error while registering: " + err);
+          return res.render("register");
+      }
+      
+      passport.authenticate("local")(req, res, function(){
+          res.redirect("/secret");
+      })
+  });
 });
 
 
